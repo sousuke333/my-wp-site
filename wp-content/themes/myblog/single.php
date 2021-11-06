0b74407cccc7cc3647dcb25ef7951a39476f8006
sousuke333 <ergo.console.log02@gmail.com>
2021-11-07 03:27:46 +0900

    add single.php header.php footer.php includes/header.php includes/footer.php

---
+++ wp-content/themes/myblog/single.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <?php get_header(); ?>
+</head>
+
+<body>
+
+  <?php get_template_part('includes/header'); ?>
+  <?php if (have_posts()): ?>
+  <?php while (have_posts()) : the_post(); ?>
+    <!-- Page Header -->
+    <?php
+      $id = get_post_thumbnail_id();
+      $img = wp_get_attachment_image_src($id);
+    ?>
+    <header class="masthead" style="background-image: url('<?php echo $img[0]; ?>')">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 col-md-10 mx-auto">
+            <div class="post-heading">
+              <h1><?php the_title(); ?></h1>
+              <span class="meta">Posted by
+                <?php the_author(); ?>
+                on <?php the_date(); ?></span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <!-- Post Content -->
+    <article>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 col-md-10 mx-auto">
+            <?php the_post_thumbnail(array(32, 32)); ?>
+            <?php the_content(); ?>
+          </div>
+        </div>
+      </div>
+    </article>
+
+    <hr>
+  <?php endwhile; ?>
+  <?php endif; ?>
+  <?php get_template_part('includes/footer'); ?>
+  <?php get_footer(); ?>
+</body>
+
+</html>
